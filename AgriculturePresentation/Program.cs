0ad2d47b4221db833a58c7db0545377c87be53ae
duygukayaa <dygkaya56@gmail.com>
2025-10-09 19:54:59 +0300

    service,dal,manager yapılarını oluşturdum.

--- AgriculturePresentation/Program.cs
+++ AgriculturePresentation/Program.cs
@@ -1,6 +1,33 @@
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.EntityFramework;
+using DataAccessLayer.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+builder.Services.AddScoped<IServiceDal, EfServiceDal>();
+
+builder.Services.AddScoped<ITeamService, TeamManager>();
+builder.Services.AddScoped<ITeamDal, EfTeamDal>();
+
+builder.Services.AddScoped<IImageService, ImageManager>();
+builder.Services.AddScoped<IImageDal, EfImageDal>();
+
+builder.Services.AddScoped<IContactService, ContactManager>();
+builder.Services.AddScoped<IConcatDal, EfContactDal>();
+
+builder.Services.AddScoped<IAnnouncementService, AnnouncementManager>();
+builder.Services.AddScoped<IAnnouncementDal, EfAnnouncementDal>();
+
+builder.Services.AddScoped<IAddressService, AddressManager>();
+builder.Services.AddScoped<IAddressDal, EfAddressDal>();
+
+builder.Services.AddDbContext<AgricultureContext>();
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        private readonly IConcatDal _concatDal;
+
+        public ContactManager(IConcatDal concatDal)
+        {
+            _concatDal = concatDal;
+        }
+
+        public void Delete(Contact t)
+        {
+            _concatDal.Delete(t);
+        }
+
+        public Contact GetById(int id)
+        {
+            return _concatDal.GetById(id);
+        }
+
+        public List<Contact> GetListAll()
+        {
+           return _concatDal.GetListAll();
+        }
+
+        public void Insert(Contact t)
+        {
+            _concatDal.Insert(t);
+        }
+
+        public void Update(Contact t)
+        {
+            _concatDal.Update(t);
+        }
+    }
+}
