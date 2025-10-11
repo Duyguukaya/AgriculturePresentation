760d28c885670fd5d847ae7d843406c2c6cf1dd9
duygukayaa <dygkaya56@gmail.com>
2025-10-11 20:46:58 +0300

    Team sayfaları oluşturuldu.

---
+++ AgriculturePresentation/Controllers/TeamController.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly ITeamService _teamService;
+
+        public TeamController(ITeamService teamService)
+        {
+            _teamService = teamService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _teamService.GetListAll();
+            return View(values);
+        }
+
+        public IActionResult AddTeam()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTeam(Team team)
+        {
+            _teamService.Insert(team);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UpdateTeam(int id)
+        {
+            var value = _teamService.GetById(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateTeam(Team team)
+        {
+            _teamService.Update(team);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteTeam(int id)
+        {
+            var value = _teamService.GetById(id);
+            _teamService.Delete(value);
+            return RedirectToAction("Index");
+        }
+    }
+}
