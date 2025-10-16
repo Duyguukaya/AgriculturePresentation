665d7f33903c69cadbdce18e27e16e35e4f9b087
duygukayaa <dygkaya56@gmail.com>
2025-10-16 13:00:08 +0300

    dashboard viewcomponentlere ayrıldı.

---
+++ AgriculturePresentation/ViewComponents/_DashboardOverwievPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class _DashboardOverwievPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
