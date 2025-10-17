e0a0938b9512866a5feb5ba61547262ac44e2480
duygukayaa <dygkaya56@gmail.com>
2025-10-17 16:05:09 +0300

    Login sayfası ve login işlemleri tamamlandı

--- AgriculturePresentation/Controllers/LoginController.cs
+++ AgriculturePresentation/Controllers/LoginController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AgriculturePresentation.Controllers
 {
@@ -9,16 +10,41 @@ namespace AgriculturePresentation.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public LoginController(UserManager<IdentityUser> userManager)
+        public LoginController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, false,false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+
+
+
 
         [HttpGet]
         public IActionResult SignUp()
---
+++ AgriculturePresentation/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgriculturePresentation.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Lütfen kullanıcı adını giriniz")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage ="Lütfen şifreyi giriniz")]
+        public string Password { get; set; }
+    }
+}
